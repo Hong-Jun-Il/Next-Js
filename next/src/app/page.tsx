6f09e7e8e488fb6833a6a512c6ea95ff66fec914
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2025-02-05 22:38:42 +0900

    save

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,14 +1,11 @@
-import { CustomInput } from "@/components/customUi/CustomInput";
-import UserFormProvider from "./_components/User/UserFormProvider";
-import CustomLabel from "@/components/customUi/CustomLabel";
-import { CustomTextarea } from "@/components/customUi/CustomTextarea";
 import { CustomImageInput } from "@/components/customUi/CustomImageInput";
 
 export default function page() {
   const handleSubmitForm = async (formData: FormData) => {
     "use server";
-    console.log(formData.get("images"));
+    console.log(formData);
   };
+
   return (
     <main className="bg-slate-200 w-full h-dvh flex justify-center items-center">
       <form action={handleSubmitForm}>
--- next/src/components/customUi/CustomImageInput.tsx
+++ next/src/components/customUi/CustomImageInput.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { ChangeEvent, ComponentProps, forwardRef, useState } from "react";
+
+const imageNameSet = new Set<string>();
 
 export const CustomImageInput = forwardRef<
   HTMLInputElement,
@@ -15,7 +18,7 @@ export const CustomImageInput = forwardRef<
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
-    const selectedFiles = Array.from(e.target.files);
+    let selectedImages = Array.from(e.target.files);
 
     // images 배열에 담긴 이미지가 최대일 경우
     if (images && maxImages && images.length >= maxImages) {
@@ -27,25 +30,35 @@ export const CustomImageInput = forwardRef<
     if (
       images &&
       maxImages &&
-      images.length + selectedFiles.length > maxImages
+      images.length + selectedImages.length > maxImages
     ) {
       alert(`${maxImages}장 이하의 이미지만 업로드 가능합니다.`);
       return;
     }
 
     // 사용자가 이미지를 한꺼번에 최대치보다 더 많이 넣은 경우
-    if (selectedFiles && maxImages && selectedFiles.length > maxImages) {
+    if (selectedImages && maxImages && selectedImages.length > maxImages) {
       alert(`${maxImages}장 이하의 이미지만 업로드 가능합니다.`);
       return;
     }
 
+    // set에 image의 name을 저장 후 걸러냄(파일 중복 방지)
+    selectedImages = selectedImages.filter((img) => {
+      if (imageNameSet.has(img.name)) {
+        return false;
+      }
+
+      imageNameSet.add(img.name);
+      return true;
+    });
+
     setImages((prev) => {
       // 기존에 images 배열에 selected가 담겨있을 경우
       if (prev && prev.length > 0) {
-        return [...prev, ...selectedFiles];
+        return [...prev, ...selectedImages];
       }
 
-      return selectedFiles;
+      return selectedImages;
     });
   };
 
@@ -58,6 +71,12 @@ export const CustomImageInput = forwardRef<
               key={img.name}
               className="relative rounded-lg aspect-square w-[100px] overflow-hidden bg-black"
             >
+              <button
+                className="top-0 right-0 text-red-500 cursor-pointer absolute z-10"
+                type="button"
+              >
+                삭제
+              </button>
               <Image src={URL.createObjectURL(img)} alt={img.name} fill />
             </li>
           ))}
@@ -65,8 +84,10 @@ export const CustomImageInput = forwardRef<
       )}
       <label
         htmlFor={id}
-        className="bg-black flex items-center justify-center aspect-square text-[18px] text-[#FDFDFD] cursor-pointer w-[100px] rounded-lg"
-        hidden={(images && maxImages && images.length >= maxImages) || false}
+        className={cn(
+          "bg-black flex items-center justify-center aspect-square text-[18px] text-[#FDFDFD] cursor-pointer w-[100px] rounded-lg",
+          images && maxImages && images.length >= maxImages ? "hidden" : ""
+        )}
       >
         +
       </label>
